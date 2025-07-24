3637f6cd9b90e390e04603a6757216e048e8d0e8
aa23-pp88194 <blackcat83www2@gmail.com>
2025-07-24 21:24:34 +0900

    .

--- snakeproject/main.cpp
+++ snakeproject/main.cpp
@@ -19,11 +19,14 @@ using namespace std;
 
 enum tileType {
 	None = 0,
-	Wall = 1
+	Apple = 1
 };
 
-char snake_map[MAP_SIZE_Y][MAP_SIZE_X] = {};
 HANDLE std_output_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+char snake_map[MAP_SIZE_Y][MAP_SIZE_X] = {};
+int maxAppleCount = 5;
+int curAppleCount = 0;
 
 void GotoXY(COORD pos) {
 	SetConsoleCursorPosition(std_output_handle, pos);
@@ -43,14 +46,42 @@ void PrintMap() {
 			switch (snake_map[i][j])
 			{
 			case None:
-				cout << "  ";
+				cout << "◇";
+				break;
+			case Apple:
+				cout << "◎";
 				break;
 			}
 		}
 		cout << "\n";
 	}
 }
+class Map {
+private:
+	char map[50][50];
+public:
+	void PrintMap() {
+		for (int i = -1; i <= MAP_SIZE_Y; ++i) {
+			for (int j = -1; j <= MAP_SIZE_X; ++j) {
+				if (i == -1 || i == MAP_SIZE_Y || j == -1 || j == MAP_SIZE_X) {
+					cout << "■";
+					continue;
+				}
 
+				switch (map[i][j])
+				{
+				case None:
+					cout << "◇";
+					break;
+				case Apple:
+					cout << "◎";
+					break;
+				}
+			}
+			cout << "\n";
+		}
+	}
+};
 class Snake
 {
 private:
@@ -60,17 +91,23 @@ private:
 	int length = 1;
 	deque<COORD> path;
 
+	bool isAlive = true;
+
 public:
 	Snake(COORD startPos, COORD startDir) {
 		curPos = startPos;
 		curDir = startDir;
 		length = 5;
+		isAlive = true;
 	}
 	~Snake() {
 
 	}
+	bool GetAlive() { return isAlive; }
 	COORD GetPos() { return curPos; }
 	void SetDir(COORD dir) {
+		if (dir.X == -curDir.X || dir.Y == -curDir.Y)
+			return;
 		curDir = dir;
 	}
 	void Next() {
@@ -80,12 +117,26 @@ public:
 		path.push_front(curPos);
 		short nextX = curPos.X + curDir.X;
 		short nextY = curPos.Y + curDir.Y;
+		if (nextX < 0 || nextX >= MAP_SIZE_X || nextY < 0 || nextY >= MAP_SIZE_Y)
+			isAlive = false;
+		for (COORD t : path) {
+			if (t.X == nextX && t.Y == nextY) {
+				isAlive = false;
+				return;
+			}
+		}
 		curPos = { nextX, nextY };
-
+		if (snake_map[curPos.Y][curPos.X] == Apple) {
+			snake_map[curPos.Y][curPos.Y] = None;
+			GotoXY({ (short)(nextX * 2 + 2), (short)(nextY) });
+			//cout << "  ";
+			curAppleCount--;
+			length++;
+		}
 	}
 	void PrintSnake() {
 		for (COORD t : path) {
-			GotoXY({ (short)(t.X * 2), (short)(t.Y) });
+			GotoXY({ (short)(t.X * 2 + 2), (short)(t.Y + 1) });
 
 			cout << "★";
 		}
@@ -105,7 +156,12 @@ int main() {
 
 	Snake snake = Snake({ 25, 25 }, { 1, 0 });
 
-	while (isAppRunning) {
+	for (int i = 0; i < 5; ++i) {
+		curAppleCount++;
+		snake_map[rand() % 51][rand() % 51] = Apple;
+	}
+
+	while (snake.GetAlive()) {
 		//키입력
 		if (_kbhit()) {
 			char key = _getch();
@@ -132,10 +188,15 @@ int main() {
 		}
 		snake.Next();
 
+		if (curAppleCount < maxAppleCount) {
+			curAppleCount++;
+			snake_map[rand() % 51][rand() % 51] = Apple;
+		}
+
 		GotoXY(0, 0);
 		PrintMap();
 		snake.PrintSnake();
-		SLP(100);
+		SLP(50);
 	}
 
 	//콘솔창 일시정지 (종료 로그 안띄우게)
